98295a4e13ec92d1aaad81be9780955a8ee41e11
mb4ndeira <mbandeira.cs@gmail.com>
2022-05-31 16:24:50 +0000

    feat: implement custom exception classes to boleto digitable line validation pipe

--- src/boletos/validate-boleto-typed-line-format.pipe.ts
+++ src/boletos/validate-boleto-typed-line-format.pipe.ts
@@ -1,18 +1,16 @@
-import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+import { Injectable, PipeTransform } from '@nestjs/common';
+
+import { ConsultedBoletoIsAtWrongFormat } from './exceptions/ConsultedBoletoIsAtWrongFormat';
 
 @Injectable()
 export class ValidateBoletoTypedLineFormatPipe implements PipeTransform {
   transform(typedLine: string): string {
     const onlyNumbersRegex = new RegExp('^[0-9]+$');
     if (!onlyNumbersRegex.test(typedLine))
-      throw new BadRequestException(
-        'Wrong boleto format: must only contain numbers',
-      );
+      throw new ConsultedBoletoIsAtWrongFormat('deve conter apenas números');
 
     if (typedLine.length !== 47)
-      throw new BadRequestException(
-        'Wrong boleto format: must contain 47 characters',
-      );
+      throw new ConsultedBoletoIsAtWrongFormat('deve conter 47 caracteres');
 
     return typedLine;
   }
