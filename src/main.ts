2ad1e254b4ad4da5a622b35be7786b1ff6b7fb05
mb4ndeira <mbandeira.cs@gmail.com>
2022-06-02 10:06:55 +0000

    feat: implement swagger recipe from nest docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('BR-payment-slip-processing')
+    .setDescription(
+      'Serviço para processamento de boletos de cobrança ou arrecadação, em acordo com as normas da FEBRABAN.',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
