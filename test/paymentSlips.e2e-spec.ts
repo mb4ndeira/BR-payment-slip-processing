38d1ff209628133afe8fda1114e6a76d49695392
mb4ndeira <mbandeira.cs@gmail.com>
2022-06-02 15:45:28 +0000

    test: add e2e tests for payment slips routes

---
+++ test/paymentSlips.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+
+import { PaymentSlipsModule } from './../src/paymentSlips/payment-slips.module';
+
+describe('PaymentSlipsControllers e2e', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [PaymentSlipsModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('GET /boleto/:digitable_line', () => {
+    const validDigitableLine =
+      '21290001192110001210904475617405975870000002000';
+
+    return request(app.getHttpServer())
+      .get(`/boleto/${validDigitableLine}`)
+      .expect(200)
+      .expect({
+        barCode: '21299758700000020000001121100012100447561740',
+        amount: '20.00',
+        expirationDate: '2018-07-16',
+      });
+  });
+});
